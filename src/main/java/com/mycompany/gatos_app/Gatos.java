100298b84a786ba97df4229f6027f913b589aca5
Marcela Neira <martaymarcela@gmail.com>
2020-11-07 19:28:51 +0100

    Iniciando al app

---
+++ src/main/java/com/mycompany/gatos_app/Gatos.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gatos_app;
+
+/**
+ *
+ * @author marta
+ */
+public class Gatos {
+    int id;
+    String url;
+    String apikey = "e653600e-3983-4e38-9bcc-eae70c3c4a3a";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+}
